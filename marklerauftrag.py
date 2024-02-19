8d66a242c4ee6fa8ad7a66694589c475372b3d6f
DBauer404 <denis.b22@gso.schule.koeln>
2024-02-19 13:14:43 +0100

    Schleife gemacht ka

--- marklerauftrag.py
+++ marklerauftrag.py
@@ -1,8 +1,23 @@
 class Raum:
-    def __init__(self, laenge, breite, name):
+    def __init__(self, laenge, breite, name, raumgroesse):
         self.name = name
         self.laenge = laenge
         self.breite = breite
         self.raumgroesse = laenge * breite
 
-raeume = input("")
+raeume = 0
+
+while True:
+    try: 
+        raeume = int(input("Hey Makler, Gebe die Anzahl an Räumen an welches das Gebäude hat: "))
+        Raum.name = str(input("Wie ist der Name deines Raumes? "))
+        Raum.laenge = int(input("Gebe die Länge deines Raumes (in Metern) an: "))
+        Raum.breite = int(input("Gebe die Breite deines Raumes (in Metern) an: "))
+        break
+    except ValueError:
+        print("Die Eingabe ist Falsch, bitte erneut eingeben.")
+
+input("einfach nur nen test")
+
+# print("Dein Gebäude hat " + str(raeume) + " Räume und der Name des Raumes lautet: " + Raum.name + " Die länge beträgt: " + str(Raum.laenge) + "m und die Breite beträgt: " + str(Raum.breite) + " Die Raumgröße ist: " {Raum.raumgroesse})
+# print("Dein Gebäude hat " + raeume + " Rä)
