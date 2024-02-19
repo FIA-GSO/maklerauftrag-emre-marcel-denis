09ed4eeb916078e1f1b05084534d1d677494766c
Emre <emre.a47@gso.schule.koeln>
2024-02-19 13:59:47 +0100

    Add ausgabe full

--- marklerauftrag.py
+++ marklerauftrag.py
@@ -10,10 +10,10 @@ def raum_abfrage(num):
     alle_raeume = []
     for i in range(num):
         try: 
-            Raum.name = str(input("Wie ist der Name deines Raumes? "))
+            Raum.name = str(input("\nWie ist der Name deines Raumes? "))
             Raum.laenge = int(input("Gebe die Länge deines Raumes (in Metern) an: "))
             Raum.breite = int(input("Gebe die Breite deines Raumes (in Metern) an: "))
-            Raum.raumgroesse = Raum.laenge * Raum.breite
+            Raum.groesse = Raum.laenge * Raum.breite
 
             alle_raeume.append(Raum)
         except ValueError:
@@ -22,15 +22,21 @@ def raum_abfrage(num):
     return alle_raeume
 
 
-def raum_ausgeben(alle_raeume, num):
+def raum_ausgeben(raeume, num):
+    print(f"Dein Gebäude hat {num}")
+
     for i in range(num):
-        print(alle_raeume[i])
+        print(f"""\n
+        Der Name des Raumes lautet: {raeume[i].name}
+        Die länge beträgt: {raeume[i].laenge}
+        und die Breite beträgt: {raeume[i].breite}.
+        Die Raumgröße ist: {raeume[i].groesse}
+        """)
 
-
-# print("Dein Gebäude hat " + str(raeume) + " Räume und der Name des Raumes lautet: " + Raum.name + " Die länge beträgt: " + str(Raum.laenge) + "m und die Breite beträgt: " + str(Raum.breite) + " Die Raumgröße ist: " {Raum.raumgroesse})
 # print("Dein Gebäude hat " + raeume + " Rä)
 
 if __name__ == '__main__':
     raeume = int(input("Hey Makler, Gebe die Anzahl an Räumen an welches das Gebäude hat: "))
     alle_raeume = raum_abfrage(raeume)
+    print(alle_raeume[0].name)
     raum_ausgeben(alle_raeume, raeume)
