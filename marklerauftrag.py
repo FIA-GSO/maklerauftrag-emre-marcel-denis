652bae9e41f8bf907abda222651cb088b1c50400
Marcel Paetschke <marcel.p25@gso.schule.koeln>
2024-03-04 21:31:52 +0100

    Teilraumabfrage verbessert

--- marklerauftrag.py
+++ marklerauftrag.py
@@ -1,8 +1,13 @@
 class Raum:
     def __init__(self, name):
         self.name = name
+        self.raeume = []
         self.teilraeume = []
         self.groesse = 0
+
+    def add_raum(self, laenge, breite):
+        self.raeume.append((laenge, breite))
+        self.groesse += laenge * breite
 
     def add_teilraum(self, laenge, breite):
         self.teilraeume.append((laenge, breite))
@@ -10,15 +15,24 @@ class Raum:
 
 def raum_abfrage(num):
     alle_raeume = []
+    aktuellerRaum = 1
     for i in range(num):
         try:
-            name = str(input("\nWie ist der Name deines Raumes? "))
+            name = str(input(f"\nWie ist der Name des {aktuellerRaum}. Raumes? "))
+            aktuellerRaum += 1
             raum = Raum(name)
-            teilraeume_num = int(input(f"Wie viele Teilräume hat der Raum '{name}'? "))
-            for _ in range(teilraeume_num):
-                laenge = float(input("Gebe die Länge eines Teilraums (in Metern) an: "))
-                breite = float(input("Gebe die Breite eines Teilraums (in Metern) an: "))
-                raum.add_teilraum(laenge, breite)
+            teilraum_abfrage = str(input("Hat dieser Raum Teilräume (Ja/Nein)\n"))
+            if teilraum_abfrage.lower() == "ja":
+                teilraeume_num = int(input(f"Wie viele Teilräume hat der Raum '{name}'? "))
+                for _ in range(teilraeume_num):
+                    laenge = float(input("Gebe die Länge eines Teilraums (in Metern) an: "))
+                    breite = float(input("Gebe die Breite eines Teilraums (in Metern) an: "))
+                    raum.add_teilraum(laenge, breite)
+            else:
+                laenge = float(input("Gebe die Länge des Raumes (in Metern) an: "))
+                breite = float(input("Gebe die Breite des Raumes (in Metern) an: "))
+                raum.add_raum(laenge, breite)
+
             alle_raeume.append(raum)
         except ValueError:
             print("Die Eingabe ist Falsch, bitte erneut eingeben.")
