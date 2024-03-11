5a37c53b8158ae9bf65cab75ae7ffe57f9f92af5
Marcel Paetschke <marcel.p25@gso.schule.koeln>
2024-03-11 12:41:53 +0100

    Kommentare hinzugefügt

--- marklerauftrag.py
+++ marklerauftrag.py
@@ -1,27 +1,33 @@
 class Raum:
+
     def __init__(self, name):
+        """Initialisiert einen Raum-Objekt."""
         self.name = name
         self.raeume = []
         self.teilraeume = []
         self.groesse = 0
 
     def add_raum(self, laenge, breite):
+        """Fügt dem Gebäude einen Raum mit gegebenen Maßen hinzu und aktualisiert die Gesamtgröße."""
         self.raeume.append((laenge, breite))
         self.groesse += laenge * breite
 
     def add_teilraum(self, laenge, breite):
+        """Fügt ggfs dem Raum einen Teilraum mit gegebenen Maßen hinzu und aktualisiert die Gesamtgröße."""
         self.teilraeume.append((laenge, breite))
         self.groesse += laenge * breite
 
+
 def raum_abfrage(num):
+    """Fragt den Benutzer nach Details für eine gegebene Anzahl von Räumen und erstellt diese."""
     alle_raeume = []
-    aktuellerRaum = 1
+    aktueller_raum = 1
     for i in range(num):
         try:
-            name = str(input(f"\nWie ist der Name des {aktuellerRaum}. Raumes? "))
-            aktuellerRaum += 1
+            name = input(f"\nWie ist der Name des {aktueller_raum}. Raumes? ")
+            aktueller_raum += 1
             raum = Raum(name)
-            teilraum_abfrage = str(input("Hat dieser Raum Teilräume (Ja/Nein)\n"))
+            teilraum_abfrage = input("Hat dieser Raum Teilräume (Ja/Nein)\n")
             if teilraum_abfrage.lower() == "ja":
                 teilraeume_num = int(input(f"Wie viele Teilräume hat der Raum '{name}'? "))
                 for _ in range(teilraeume_num):
@@ -35,11 +41,13 @@ def raum_abfrage(num):
 
             alle_raeume.append(raum)
         except ValueError:
-            print("Die Eingabe ist Falsch, bitte erneut eingeben.")
+            print("Die Eingabe ist falsch, bitte erneut eingeben.")
 
     return alle_raeume
 
+
 def raum_ausgeben(alle_raeume):
+    """Gibt Informationen über alle Räume und die Gesamtfläche aus."""
     gesamtflaeche = sum(raum.groesse for raum in alle_raeume)
     print(f"Dein Gebäude hat insgesamt {len(alle_raeume)} Räume und eine Gesamtfläche von {gesamtflaeche}m².")
 
@@ -49,6 +57,7 @@ def raum_ausgeben(alle_raeume):
         Anzahl der Teilräume: {len(raum.teilraeume)}
         Gesamtfläche: {raum.groesse}m²
         """)
+
 
 if __name__ == '__main__':
     raeume = int(input("Wie viele Räume hat das Gebäude?: "))
